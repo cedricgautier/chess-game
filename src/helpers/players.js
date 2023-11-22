dea771a319a5c6b4e8340fb5bd23f0095c50222c
Cedric Gautier <cedric.gautier@qonto.com>
2023-11-22 04:41:42 +0100

    Add helpers for players

---
+++ src/helpers/players.js
@@ -0,0 +1,56 @@
+import { pieces as p } from "../constants/pieces"
+
+export const getPlayerSymbolColor = (
+  whiteSymbol,
+  blackSymbol,
+  playerColor = playerColor.toLowerCase()
+) => {
+  if (playerColor === "white") {
+    return whiteSymbol
+  }
+
+  if (playerColor === "black") {
+    return blackSymbol
+  }
+
+  return "❌"
+}
+
+export const getDefaultPlayerInfo = (playerColor) => ({
+  moves: 0,
+  piecesEaten: 0,
+  kingDefeated: false,
+  pieces: {
+    totalOfPieces: 16,
+    king: {
+      total: 1,
+      symbol: getPlayerSymbolColor(p.WHITE_KING, p.BLACK_KING, playerColor),
+      position: ""
+    },
+    queen: {
+      total: 1,
+      symbol: getPlayerSymbolColor(p.WHITE_QUEEN, p.BLACK_QUEEN, playerColor),
+      position: ""
+    },
+    rook: {
+      total: 2,
+      symbol: getPlayerSymbolColor(p.WHITE_ROOK, p.BLACK_QUEEN, playerColor),
+      position: ""
+    },
+    bishop: {
+      total: 2,
+      symbol: getPlayerSymbolColor(p.WHITE_BISHOP, p.BLACK_BISHOP, playerColor),
+      position: ""
+    },
+    knight: {
+      total: 2,
+      symbol: getPlayerSymbolColor(p.WHITE_KNIGHT, p.BLACK_KNIGHT, playerColor),
+      position: ""
+    },
+    pawn: {
+      total: 8,
+      symbol: getPlayerSymbolColor(p.WHITE_PAWN, p.BLACK_PAWN, playerColor),
+      position: ""
+    }
+  }
+})
